f323cf00e973adb63c15056459d4947d49ca5990
PooYooGitHub <ysy3083446265@gmail.com>
2024-07-07 18:17:00 +0800

    complete tabBar

--- main.js
+++ main.js
@@ -13,6 +13,33 @@ app.$mount()
 
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
+import { $http } from '@escook/request-miniprogram'
+//发送请求和获取请求的配置信息
+uni.$http = $http
+
+// 配置请求根路径
+$http.baseUrl = 'https://result.eolink.com/'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 export function createApp() {
   const app = createSSRApp(App)
   return {
